822d6fd6ea1cbd2c4b01b93e2861f971944c6522
David Taccetta <taccetta14@gmail.com>
2022-05-31 14:39:24 -0400

    Initial VatiableExcercised commit

---
+++ VariablesExcercise/VariablesExcercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myCat = "Toeny";
+            int catsToes = 28;
+            char confusion = '?';
+            bool polydactyly = true;
+            double vetVisits = 2.7;
+            decimal toeConversations = 99.999m;
+
+            Console.WriteLine($"My cat {myCat} has {catsToes} toes. You may be thinking {catsToes} toes{confusion} Well, it's because {myCat} is is a polydactyly. {polydactyly}");
+            Console.WriteLine($"This makes us have to go to the vet quite often. Usually about {vetVisits} times a month to get his paws trimmed.");
+            Console.WriteLine($"We also usually have about {toeConversations} coversations about his toes when we bring him in.");
+        }
+    }
+}
