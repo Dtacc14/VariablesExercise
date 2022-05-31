67b3875c9dc24520888cfffc63133069782d0af8
David Taccetta <taccetta14@gmail.com>
2022-05-31 15:13:09 -0400

    new change

--- VariablesExcercise/VariablesExcercise/Program.cs
+++ VariablesExcercise/VariablesExcercise/Program.cs
@@ -19,7 +19,7 @@ namespace VariablesExcercise
             decimal toeConversations = 99.999m;
 
             Console.WriteLine($"My cat {myCat} has {catsToes} toes. You may be thinking {catsToes} toes{confusion} Well, it's because {myCat} is is a polydactyly.");
-            Console.WriteLine($"It is {polydactyly} {myCat} has that many toes.");
+            Console.WriteLine($"It is {polydactyly} {myCat} has that many toes!");
             Console.WriteLine($"This makes us have to go to the vet quite often. Usually about {vetVisits} times a month to get his paws trimmed.");
             Console.WriteLine($"We also usually have about {toeConversations} coversations about his toes when we bring him in.");
         }
